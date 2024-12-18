9b32fe65b404fea9ec4f53ae723f92cec0e4dc72
marcusryden92 <marcus.ryden1992@gmail.com>
2024-12-18 15:13:49 +0100

    Moved hooks underneath /app

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "@/globals.css";
 
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
